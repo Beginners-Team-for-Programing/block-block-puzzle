16a6aac6a42f6d9cb3452b2c27a60d8a888161a4
Junpei <dpsknu25@gmail.com>
2021-01-26 21:38:41 +0900

    20210126

--- build/app.js
+++ build/app.js
@@ -7,14 +7,55 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+var x = canvas.width / 2;
+var y = canvas.height - 30;
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+var dx = 2;
+var dy = -2;
+
+function draw()
+{
+    ctx.beginPath();
+    ctx.arc(x, y, 20, 0, Math.PI * 2, false);
+    ctx.fillStyle = "green";
+    ctx.fill();
+    ctx.closePath();
+
+    x += dx;
+    y += dy;
+
+}
+
+setInterval(draw, 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Canvas上に図形を描写
+
+// ctx.beginPath();
+// ctx.rect(20, 40, 50, 50);
+// ctx.fillStyle = "#FF0000";
+// ctx.fill();
+// ctx.closePath();
+
+// ctx.beginPath();
+// ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
+// ctx.fillStyle = "green";
+// ctx.fill();
+// ctx.closePath();
+
+// ctx.beginPath();
+// ctx.rect(160, 10, 100, 40);
+// ctx.strokeStyle = "raba(0, 0, 255, 0.5)";
+// ctx.stroke();
+// ctx.closePath();
